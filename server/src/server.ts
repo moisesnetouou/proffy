bb161d3dc745d4ea793da20e6067b427cdef63af
Moisés <moisesnetored@gmail.com>
2022-07-12 15:11:27 -0400

    feat: Class creation route successfully completed.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-  return response.send("Acessou a rota")
-})
+app.use(routes);
 
 app.listen(3333);
